6808e4f14ce5e0904ecff8b00a60e6b428448dd0
Hiromichi Yamada <yamada@torques.jp>
2014-09-29 17:32:20 +0900

    Initial Commit

---
+++ CamTester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CamTester
+//
+//  Created by 山田宏道 on 2014/09/29.
+//  Copyright (c) 2014年 Torques Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
